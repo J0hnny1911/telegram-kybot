2183f27a9373af593b77dc81ef587a0974868651
kenniki <kenniki@me.com>
2014-03-26 14:46:18 +0100

    help now shows the commands, not their handlers.

--- src/modules/help.class.php
+++ src/modules/help.class.php
@@ -4,11 +4,11 @@ class help {
 	public $name 	= 'help';
 	public $version = '0.01';
 	public $creator = 'kenny';
-	public $desc	= 'Tells you your';
+	public $desc	= 'Shows a list of available commands.';
 	
 	public function handle ($recv, $replyobj) {
-		$helptxt = implode(", ", $replyobj->commands);
-		$replyobj->reply('txt','Available commands: '.$helptxt);
+		$helptxt = implode(", ", array_keys($replyobj->commands));
+		$replyobj->reply('txt','Available commands: '.$helptxt.'. Additional commands may be available depending on context.');
 	}
 	
 }
