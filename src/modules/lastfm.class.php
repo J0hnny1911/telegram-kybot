b8678cbed521dbffc9d63305d0cf59f3a51c4ff3
kenniki <kenniki@me.com>
2014-08-23 00:29:40 +0200

    Completely restructured the lastfm module, wwhich is now much more reliable and doesn't crash the bot anymore. Thanks to @philipnorton42 for the code.

--- src/modules/lastfm.class.php
+++ src/modules/lastfm.class.php
@@ -2,43 +2,20 @@
 class lastfm {
 
 	public $name 	= 'lastfm';
-	public $version = '0.01';
+	public $version = '0.02';
 	public $creator = 'kenny';
 	public $desc	= 'Tells you what a lastfm account is currently listening to.';
 	
-	public  function rss_to_array($tag, $array, $url) {
-                $doc = new DOMdocument();
-                $doc->load($url);
-                $rss_array = array();
-                $items = array();
-                foreach($doc->getElementsByTagName($tag) AS $node) {    
-                        foreach($array AS $key => $value) {
-                                $items[$value] = $node->getElementsByTagName($value)->item(0)->nodeValue;
-                        }
-                        array_push($rss_array, $items);
-                }
-                return $rss_array;
-    }
-	
 	public function handle ($recv, $replyobj) {
-		        
-        $rss_tags = array(
-                'title',
-                'link',
-                'guid',
-                'comments',
-                'description',
-                'pubDate',
-                'category',
-        );
-        $rss_item_tag = 'item';
-        $rss_url = 'http://ws.audioscrobbler.com/1.0/user/'.$recv[1].'/recenttracks.rss';
-        
-        $rssfeed = $this->rss_to_array($rss_item_tag,$rss_tags,$rss_url);
-        
-		//echo str_replace("?","-",utf8_decode($rssfeed[0][title]));
-
-		$replyobj->reply('txt',str_replace("?","-",utf8_decode($rssfeed[0][title])));
+		// Thanks to @philipnorton42 for the code
+		$scrobbler_url = "http://ws.audioscrobbler.com/2.0/user/" . $recv[1] . "/recenttracks";
+		if ($scrobbler_xml = file_get_contents($scrobbler_url)) {
+	        $scrobbler_data = simplexml_load_string($scrobbler_xml);        
+	        $replyobj->reply('txt',$recv[1] . ' most recently listened to '. $scrobbler_data->track[0]->name .' by '. $scrobbler_data->track[0]->artist .'.');
+		} else {
+			$replyobj->reply('txt',"Sorry, this user doesn't exist or hasn't listened to any music yet.");
+		}
+		
 	}
 	
 }
